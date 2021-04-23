846fea3f57d5ee9dd8a6d8c11261c18e33e5954c
Emmett Young <emmettyoung92@gmail.com>
2021-04-23 14:55:57 +0800

    Create maven project with spock

---
+++ app/src/test/groovy/com/mettyoung/ExampleSpec.groovy
@@ -0,0 +1,10 @@
+package com.mettyoung
+
+import spock.lang.Specification
+
+class ExampleSpec extends Specification {
+    def "should be a simple assertion"() {
+        expect:
+        1 == 0
+    }
+}
